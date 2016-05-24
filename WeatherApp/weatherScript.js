3e1d850bfc293833002de55c9a686c687e14956c
Teleisha Hall <nickyb63@hotmail.com>
2016-05-23 22:30:25 -0400

    used API's to retrieve weather data in JavaScript

---
+++ WeatherApp/weatherScript.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+
+    var lat ;  // create empty variable to store user's lattitude location
+    var long;  // create empty variable to store user's lattitude location
+
+    var api = "http://ip-api.com/json";    // api to retrieve user's location using IP address and return JSON format
+    $.getJSON(api, function (data) {
+        lat = data.lat;     //  store lattitude
+        long = data.lon;    // store longitude
+
+        var api = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=23b2dd88660300b91334a54653a4f9cc";
+        // OpenWeatherMap API url
+
+        $.getJSON(api, function (data) {  // retrieve necessary data from api url above
+            //$("#weatherIcon").html();     
+            $("#temp").html("Temp: " + data.main.temp); // display temperature in html section
+            $("#location").html(data.name);        // display location city in html section
+            // $("#conditions").html(data.weather);
+        });
+
+    });
+
+
+});
